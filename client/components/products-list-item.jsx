44c14e592177fad148b9d0296d4b3a7a33e98923
jaimegsandoval <jaime.devs@gmail.com>
2020-09-27 18:58:17 -0700

    Added dollar sign for product price

--- client/components/products-list-item.jsx
+++ client/components/products-list-item.jsx
@@ -7,7 +7,7 @@ function ProductListItem(props) {
         <img className="my-card-img-top card-img-top" src={props.products.image} alt="Image of product" />
         <div className="my-card-body card-body">
           <h5 className="my-card-title card-title">{props.products.name}</h5>
-          <p className="price text-secondary">{props.products.price}</p>
+          <p className="price text-secondary">{`$${props.products.price}`}</p>
           <p className="my-card-text card-text pb-5">{props.products.shortDescription}</p>
         </div>
       </div>
