e11c7520dde8d9317943663ad9ad28bd46f8c788
jaimegsandoval <jaime.devs@gmail.com>
2020-10-14 14:11:10 -0700

    Added styles and icon for the header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Header(props) {
 
   return (
-    <header className="header bg-secondary">
+    <header className="header">
       <div className="col-sm-12 col-md-10 m-auto d-flex align-items-center">
-        <i className="header-dollar-sign text-white fas fa-dollar-sign mr-2"></i>
-        <p className="py-3 text-white">Wicked Sales</p>
+        <img className="bonsai-icon bg-white" src="images/bonsai-icon.png" />
+        <p className="py-4 text-white nav-title ml-2">Bonsai Depot</p>
         <div className="cart-box d-inline-block ml-auto pr-5">
           <span className="cart text-white pr-3">{`${props.cartItemCount} Items`}</span>
           <i className="fas fa-shopping-cart text-white" onClick={() => props.setView('cart', {})}></i>
