affd0e5da3024f45dda7bb2436d2924152bc8d5e
jaimegsandoval <jaime.devs@gmail.com>
2020-10-14 19:34:47 -0700

    Changed data in DB for products and changed styles for desktop

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,8 +6,8 @@ function Header(props) {
     <header className="header">
       <div className="col-sm-12 col-md-10 m-auto d-flex align-items-center">
         <img className="bonsai-icon bg-white" src="images/bonsai-icon.png" />
-        <p className="py-4 text-white nav-title ml-2">Bonsai Depot</p>
-        <div className="cart-box d-inline-block ml-auto pr-5">
+        <a className="py-3 text-white nav-title ml-2 text-decoration-none pointer">Bonsai Depot</a>
+        <div className="cart-box d-inline-block ml-auto px-5 py-4 checkout-cart">
           <span className="cart text-white pr-3">{`${props.cartItemCount} Items`}</span>
           <i className="fas fa-shopping-cart text-white" onClick={() => props.setView('cart', {})}></i>
         </div>
--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -28,21 +28,22 @@ class ProductDetails extends React.Component {
     return (
       <>
         <div className="container">
-          <div className="back-to-catalog p-1 pb-2 mb-5 align-middle text-center border border-secondary col-6 col-md-2" onClick={() => this.props.setView('catalog', {})}>Back to Catalog</div>
+          <div className="back-to-catalog p-1 pb-2 mb-5 align-middle text-center text-white col-6 col-md-2" onClick={() => this.props.setView('catalog', {})}>Back to Catalog</div>
           <div className="row product-image-row">
             <div className="product-image-box mb-5 col-md-6">
               <img className="product-image" src={this.state.product.image} alt="Image of product" />
+
             </div>
 
-            <div className="product-short-detail-box col-md-6">
+            <div className="product-short-detail-box col-md-6 text-white">
               <p className="product-detail-title">{this.state.product.name}</p>
-              <p className="product-price text-secondary">{`${formatter.format(this.state.product.price / 100)}`}</p>
+              <p className="product-price text-white">{`${formatter.format(this.state.product.price / 100)}`}</p>
               <p className="product-short-description mb-5">{this.state.product.shortDescription}</p>
-              <button onClick={() => this.props.addToCart(this.state.product)} className="add-to-btn btn-primary text-white rounded py-3 px-4">Add to Cart</button>
+              <button onClick={() => this.props.addToCart(this.state.product)} className="add-to-btn py-3 px-4">Add to Cart</button>
             </div>
           </div>
 
-          <div className="row product-long-description px-3">{this.state.product.longDescription}</div>
+          <p className="row product-long-description px-3 text-white">{this.state.product.longDescription}</p>
 
         </div>
       </>
